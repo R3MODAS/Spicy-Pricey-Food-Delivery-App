db422c0d1c2d605d6d84369e7161f7e0249f6b6e
Sharadindu Das <remodas7774@gmail.com>
2023-11-21 22:18:41 +0530

    changes done

--- src/components/CartList.jsx
+++ src/components/CartList.jsx
@@ -1,22 +1,38 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import { MENU_IMG } from '../utils/constants';
 import { Link } from 'react-router-dom';
 import { useDispatch } from 'react-redux';
 import { deleteItem } from '../utils/cartSlice';
 import toast, { Toaster } from 'react-hot-toast';
-import ItemPrice from './ItemPrice';
 
 const CartList = (props) => {
     const { items } = props;
     const dispatch = useDispatch();
-    const [TotalPrice, setTotalPrice] = useState(0);
+    const [TotalCost, setTotalCost] = useState(0);
+
+    useEffect(() => {
+
+        const updatedItemPrice = items?.map((card) => (
+            parseInt(
+                card?.card?.info?.price ?
+                    card?.card?.info?.price : card?.card?.info?.defaultPrice
+            ) / 100
+        ))
+
+        const TotalItemPrice = updatedItemPrice?.reduce((total, currPrice) => (
+            total + currPrice
+        ), 0)
+
+        setTotalCost(TotalItemPrice);
+
+    }, [])
 
     const handleDeleteItem = (item) => {
         dispatch(deleteItem(item?.card?.info?.id));
         toast.success('Removed from the Cart', {
-            className : "font-ProximaNovaSemiBold",
-            position : "top-center",
-            duration : 1500
+            className: "font-ProximaNovaSemiBold",
+            position: "top-center",
+            duration: 1500
         });
     }
 
@@ -45,13 +61,20 @@ const CartList = (props) => {
                                             {
                                                 item?.card?.info?.imageId && <img src={MENU_IMG + item?.card?.info?.imageId} alt="menu-img" className="object-cover w-full h-full sm:w-[150px] sm:h-[96px] rounded-lg" />
                                             }
-                                            <button className="w-16 h-7 text-xs md:w-24 md:h-9 bg-red-500 text-white rounded addBtn font-ProximaNovaBold uppercase md:text-sm cursor-pointer absolute bottom-0 left-1/2 -translate-x-1/2" 
-                                onClick={() => handleDeleteItem(item)}>Remove</button>
+                                            <button className="w-16 h-7 text-xs md:w-24 md:h-9 bg-red-500 text-white rounded addBtn font-ProximaNovaBold uppercase md:text-sm cursor-pointer absolute bottom-0 left-1/2 -translate-x-1/2"
+                                                onClick={() => handleDeleteItem(item)}>Remove</button>
                                         </div>
                                     </div>
                                 </div>
                             ))
                         }
+
+                        <div className='flex items-center justify-center flex-col sm:flex-row'>
+                            <h2 className='text-center bg-orange-500 text-white w-60 mt-5 font-ProximaNovaSemiBold text-lg flex items-center h-14  gap-3 justify-center'>Total Price : <span className='rupee'>{TotalCost}</span></h2>
+
+                            <button className='bg-black text-white h-14 w-60 mt-5 font-ProximaNovaMed text-lg'>Continue to Checkout</button>
+                        </div>
+
                     </> :
 
                     <div className='flex items-center justify-center flex-col'>
@@ -60,7 +83,7 @@ const CartList = (props) => {
                         <p className='mt-2 text-[#7e808c] font-ProximaNovaThin text-sm'>You can go to home page to view more restaurants</p>
                         <Link to="/" className='uppercase bg-orange-500 text-white font-ProximaNovaSemiBold mt-4 px-5 py-[11px] cursor-pointer text-[15px]'>see restaurants near you</Link>
                     </div>
-                
+
 
             }
 
