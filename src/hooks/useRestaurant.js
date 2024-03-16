70497765f5ee8e06e03cbbe6c3291fd836b0248e
Sharadindu Das <remodas7774@gmail.com>
2024-03-16 15:14:38 +0530

    some changes done

--- src/hooks/useRestaurant.js
+++ src/hooks/useRestaurant.js
@@ -11,7 +11,7 @@ const useRestaurant = () => {
     const lat = userLocation?.lat ? userLocation?.lat : 12.9715987
     const lng = userLocation?.lng ? userLocation?.lng : 77.5945627
 
-    const RES_API = `/dapi/restaurants/list/v5?lat=${lat}&lng=${lng}&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING`
+    const RES_API = `api/proxy/swiggy/dapi/restaurants/list/v5?lat=${lat}&lng=${lng}&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING`
 
     useEffect(() => {
         fetchRestaurantData()
@@ -19,7 +19,7 @@ const useRestaurant = () => {
 
     const fetchRestaurantData = async () => {
         try {
-            const response = await fetch(RES_API);
+            const response = await fetch(import.meta.env.VITE_BASE_URL + RES_API);
             if (!response.ok) {
                 const err = response.status;
                 throw new Error(err)
--- vite.config.js
+++ vite.config.js
@@ -3,14 +3,5 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  server: {
-    proxy: {
-      '/dapi': {
-        target: 'https://www.swiggy.com',
-        changeOrigin: true,
-        pathRewrite: { '^/dapi': '/dapi' },
-      },
-    },
-  }
+  plugins: [react()]
 })
